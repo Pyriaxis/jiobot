0c312ed8514bf0a2c802eb2134783d5a5a318dfe
Roger Ong <winter.scaze@gmail.com>
2016-08-30 21:29:22 +0800

    Updated with add/remove options to mio

--- bot.js
+++ bot.js
@@ -55,7 +55,7 @@ bot.on('/new', msg => {
     jioMembers.findOne({telegramId: msg.from.id}).then(doc =>{
         if (doc !== null){
             if (creating[msg.from.id] !== undefined){
-                return bot.sendMessage(msg.from.id, 'You are already in process of creating a Jio. Please finish or cancel that Jio first.')
+                return bot.sendMessage(msg.from.id, 'You are already in process of creating/editing a Jio. Please finish or cancel that Jio first.')
             } else {
 
                 jioDB.insert({
@@ -125,7 +125,42 @@ bot.on('ask.meetupOptions', msg => {
         ['/finishJio']
     ],{resize: true, once: true});
 
-    return bot.sendMessage(msg.chat.id, 'Ok, option added. Please continue adding options or press the done button.', {markup, ask: 'meetupOptions'});
+    return bot.sendMessage(msg.chat.id, 'Ok, option added. Please continue adding options or press the /finishJio button.', {markup, ask: 'meetupOptions'});
+});
+
+bot.on('ask.editOptions', msg => {
+
+    if (msg.text === '/finishJio') {
+
+        let inlineArray = [];
+
+        //group workflow
+        return jioDB.findOne({_id: creating[msg.from.id]}).then(doc => {
+            for (let i = 0; i < doc.options.length; i++){
+                inlineArray.push([bot.inlineButton(doc.options[i].optionName, {callback: JSON.stringify({ id: doc._id ,optionName: doc.options[i].optionName}) }) ]);
+            }
+
+            delete(creating[msg.from.id]);
+
+            let markup = bot.inlineKeyboard(inlineArray);
+            return bot.sendMessage(doc.groupId, 'Jio for ' + doc.title + ' has been edited by ' + doc.creator + '!\n' +
+                'Please choose from the current options below:', { markup });
+        });
+    }
+
+    jioDB.findOne({_id: creating[msg.from.id]}, 'options').then(doc =>{
+        var newOptions = doc.options;
+        newOptions.push({optionName: msg.text, voters: []});
+        jioDB.update({_id: creating[msg.from.id]}, {$set:{options: newOptions}});
+    }).catch( err =>{
+        console.log('editOptions error');
+    }).then(()=> {db.close()});
+
+    let markup = bot.keyboard([
+        ['/finishJio']
+    ],{resize: true, once: true});
+
+    return bot.sendMessage(msg.chat.id, 'Ok, option added. Please continue adding options or press the /finishJio button.', {markup, ask: 'editOptions'});
 });
 
 // bot.on('inlineQuery', msg => {
@@ -163,45 +198,98 @@ bot.on('callbackQuery', msg =>{
     console.log(msg);
 
     var json = JSON.parse(msg.data);
-    var id = json.id;
-    var cboption = json.optionName;
 
-    return jioDB.findOne({_id: monk.id(json.id)}).then(doc => {
-        let updatedOptions = doc.options;
-        let optionIndex =  _.findIndex(updatedOptions, {optionName: cboption});
+    if (json.add) {
+        //add options
+        creating[msg.from.id] = json.add;
+        return bot.sendMessage(msg.from.id, 'Now send me a list of options to add, one by one.', {ask: 'editOptions'})
 
+    } else if (json.remove){
+        return jioDB.findOne({_id: monk.id(json.remove)}).then(doc => {
 
-        if (optionIndex !== -1){
-            //success
-            let voteIndex = _.findIndex(updatedOptions[optionIndex].voters, {id: msg.from.id});
-
-            if (voteIndex === -1){
-                updatedOptions[optionIndex].voters.push({id: msg.from.id, name: msg.from.first_name + " " +  (msg.from.last_name || "")});
-                jioDB.update({_id: monk.id(json.id)}, {$set:{options: updatedOptions}}); //todo: fix concurrency issue
-
-                let inlineArray = [];
-
-                for (let j = 0; j < doc.options.length; j++){
-                    inlineArray.push([bot.inlineButton(doc.options[j].optionName + ' - ' + doc.options[j].voters.length,
-                        {callback: JSON.stringify({ id: doc._id ,optionName: doc.options[j].optionName}) }) ]);
-                }
-                let markup = bot.inlineKeyboard(inlineArray);
-
-                let chatId = msg.message.chat.id;
-                let messageId = msg.message.message_id;
-
-                return bot.editMarkup({chatId, messageId}, {markup});
-
-            } else {
-
-                //alr voted
-                return;
+            let inlineArray = [];
+            for (let j = 0; j < doc.options.length; j++) {
+                inlineArray.push([bot.inlineButton(doc.options[j].optionName, {
+                        callback: JSON.stringify({
+                            ro:"1",
+                            id:doc._id,
+                            o:doc.options[j].optionName
+                        })
+                })]);
             }
-        } else {
-            //failure
-            return; //optionally let bot send a message;
-        }
-    });
+
+            let markup = bot.inlineKeyboard(inlineArray);
+
+            return bot.sendMessage(msg.from.id, "Click on the option you want removed.",{markup});
+        });
+    } else if (json.ro) {
+        return jioDB.findOne({_id: monk.id(json.id)}).then(doc => {
+                var newOptions = doc.options;
+                newOptions = _.without(newOptions, _.find(newOptions, {optionName: json.o}));
+                return jioDB.update({_id: monk.id(json.id)}, {$set:{options: newOptions}}).then(function(upddoc){
+                    let inlineArray = [];
+                    for (let j = 0; j < newOptions.length; j++) {
+                        inlineArray.push([bot.inlineButton(newOptions[j].optionName, {
+                            callback: JSON.stringify({
+                                ro:"1",
+                                id:json.id,
+                                o:newOptions[j].optionName
+                            })
+                        })]);
+                    }
+
+                    let chatId = msg.message.chat.id;
+                    let messageId = msg.message.message_id;
+
+                    let markup = bot.inlineKeyboard(inlineArray);
+
+                    bot.editMarkup({chatId, messageId}, {markup});
+
+                    return bot.sendMessage(msg.from.id, json.o + " has been removed." );
+                });
+        });
+    } else {
+        //user press response button
+        var id = json.id;
+        var cboption = json.optionName;
+
+        return jioDB.findOne({_id: monk.id(json.id)}).then(doc => {
+            let updatedOptions = doc.options;
+            let optionIndex =  _.findIndex(updatedOptions, {optionName: cboption});
+
+
+            if (optionIndex !== -1){
+                //success
+                let voteIndex = _.findIndex(updatedOptions[optionIndex].voters, {id: msg.from.id});
+
+                if (voteIndex === -1){
+                    updatedOptions[optionIndex].voters.push({id: msg.from.id, name: msg.from.first_name + " " +  (msg.from.last_name || "")});
+                    jioDB.update({_id: monk.id(json.id)}, {$set:{options: updatedOptions}}); //todo: fix concurrency issue
+
+                    let inlineArray = [];
+
+                    for (let j = 0; j < doc.options.length; j++){
+                        inlineArray.push([bot.inlineButton(doc.options[j].optionName + ' - ' + doc.options[j].voters.length,
+                            {callback: JSON.stringify({ id: doc._id ,optionName: doc.options[j].optionName}) }) ]);
+                    }
+                    let markup = bot.inlineKeyboard(inlineArray);
+
+                    let chatId = msg.message.chat.id;
+                    let messageId = msg.message.message_id;
+
+                    return bot.editMarkup({chatId, messageId}, {markup});
+
+                } else {
+
+                    //alr voted
+                    return;
+                }
+            } else {
+                //failure
+                return; //optionally let bot send a message;
+            }
+        });
+    }
 });
 
 bot.on('/showJio', msg => {
@@ -225,28 +313,30 @@ bot.on('/showJio', msg => {
 
 bot.on('/checkMyJio', msg => {
     // Find the list of jios created by sender in this group chat
-    return jioDB.find({groupId: msg.chat.id}).then(doc =>{
-        if (doc){
-            return jioDB.find({creatorId: msg.from.id}).then(myJioList => {
-                for (var i = 0; i < myJioList.length; i++){
-                    let inlineArray = [];
-                    for (let j = 0; j < myJioList[i].options.length; j++){
-                        inlineArray.push([bot.inlineButton(myJioList[i].options[j].optionName + ' - ' + myJioList[i].options[j].voters.length,
-                            {callback: JSON.stringify({ id: myJioList[i]._id ,optionName: myJioList[i].options[j].optionName}) }) ]);
-                    }
-                    inlineArray.push([bot.inlineButton('Edit Options', {callback: JSON.stringify({showId: myJioList[i].id})}),
-                        bot.inlineButton('Delete Jio', {callback: '/deleteJio'})]);
-
-                    let markup = bot.inlineKeyboard(inlineArray);
-                    bot.sendMessage(myJioList[i].groupId, 'Jio for ' + myJioList[i].title + ' created by ' + myJioList[i].creator + '!\n' +
-                        'Please choose from the options below:', { markup });
+        return jioDB.find({creatorId: msg.from.id}).then(myJioList => {
+            for (var i = 0; i < myJioList.length; i++){
+                let inlineArray = [];
+                for (let j = 0; j < myJioList[i].options.length; j++){
+                    inlineArray.push([bot.inlineButton(myJioList[i].options[j].optionName + ' - ' + myJioList[i].options[j].voters.length,
+                        {callback: JSON.stringify({ id: myJioList[i]._id ,optionName: myJioList[i].options[j].optionName}) }) ]);
                 }
-            });
-        } else {
+                inlineArray.push(
+                    [bot.inlineButton('Add Options', {callback: JSON.stringify({add: myJioList[i]._id})}),
+                     bot.inlineButton('Remove Options', {callback: JSON.stringify({remove: myJioList[i]._id})})],
+                    [bot.inlineButton('Delete Jio', {callback: '/deleteJio'})]
+                );
 
-        }
-    });
+                let markup = bot.inlineKeyboard(inlineArray);
+                bot.sendMessage(msg.from.id, 'Jio for ' + myJioList[i].title + ' created by ' + myJioList[i].creator + '!\n' +
+                    'Please choose from the options below:', { markup });
+            }
+        });
 });
+
+bot.on('/editOptions', msg => {
+
+
+})
 
 // bot.on('/deleteJio', msg => {
 //
